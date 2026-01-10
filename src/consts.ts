f21db80a4dbdce904369e9876e3d93fda97ee7a0
Ben Godfrey <ben@ben2.com>
2026-01-10 17:08:18 +0000

    Bower Power Schematic Super Blog

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Ben Godfrey";
-export const SITE_DESCRIPTION = "Hi, I'm Ben Godfrey. I'm a software engineering leader, ex-Founder, ex-Meta. I like to make things.";
