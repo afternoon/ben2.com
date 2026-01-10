35d14b88254e58db62b1912f48e3305cb76c6cb5
Ben Godfrey <ben@ben2.com>
2026-01-10 17:08:18 +0000

    Bower Power Schematic Super Blog

--- README.md
+++
@@ -1,3 +0,0 @@
-# ben2.com
-
-Source for my website, https://ben2.com. The site is statically-generated with [Astro](https://astro.build).
--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Ben Godfrey";
-export const SITE_DESCRIPTION = "Hi, I'm Ben Godfrey. I'm an Engineering Manager at Meta. I like to make things.";
