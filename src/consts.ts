ddb91960a28f4ffe83eaed4509072f8e49391f24
Ben Godfrey <ben@ben2.com>
2026-07-20 14:21:47 +0100

    Update title and hero photo
    I'm the hero!

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Ben Godfrey";
-export const SITE_DESCRIPTION = "Hi, I'm Ben Godfrey. I'm an Engineering Manager at Meta. I like to make things.";
+export const SITE_DESCRIPTION = "Hi, I'm Ben Godfrey. I'm a software engineering leader, ex-Founder, ex-Meta. I like to make things.";
